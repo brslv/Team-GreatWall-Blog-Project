6c185e6f4d21eb4e91314832594bedd4a8409030
brslv <brslvg@gmail.com>
2015-04-18 12:25:12 +0300

    Added page view.

---
+++ app/views/pageView.php
@@ -0,0 +1,16 @@
+<?php require_once '../app/incl/header-tiny.php'; ?> 
+
+    <main class="main container row">
+        <section class="twelve columns">
+            <article>
+                <div><h2>Sample title</h2></div>
+                
+                <div class="page-content">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </div>
+            </article>
+            </div>
+        </section>
+    </main>
+    
+<?php require_once '../app/incl/footer.php'; ?>
