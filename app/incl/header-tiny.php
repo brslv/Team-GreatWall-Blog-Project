73c98357ca2aa8b779eea85e5c28b4442f371783
brslv <brslvg@gmail.com>
2015-04-18 12:14:28 +0300

    Added a tiny-header, used everywhere, except on the homepage.

---
+++ app/incl/header-tiny.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <meta charset="utf-8" />
+    <title>This is the blog.</title>
+    
+    <!-- HARDCODE THE STYLE URL: -->
+    <link rel="stylesheet" href="http://localhost/sites/SUProject/1/public/css/style.css" type="text/css" />
+<body>
+    <header class="header u-text-center">
+        <div class="container">
+            <nav class="top-nav nav">
+                <ul class="u-upper u-bold">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Contacts</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
