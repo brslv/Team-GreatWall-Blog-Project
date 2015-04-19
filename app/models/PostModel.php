4c8f9a0b5c7683c3962002295691c14d4d30e18e
MeNinja <borislavkirilov@yahoo.com>
2015-04-19 14:11:31 +0300

    Fixed UserModule being empty

--- app/models/PostModel.php
+++ app/models/PostModel.php
@@ -1,16 +1,28 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class PostModel extends Db {
+    protected $db;
 
-/**
- * Description of PostModel
- *
- * @author brslv
- */
-class PostModel {
-//    $db = new PDO('mysql:host=' . Conf::get('paths', 'views'));
+    public function __construct() {
+
+        $this->db = new Db();
+
+
+
+    }
+    public function addPost(){
+
+        if(!empty($_POST['postTitle']) && !empty($_POST['postContent'])) {
+            $insertInfo = $this->db->prepare('INSERT INTO posts(title,content) VALUE (:postTitle,:postContent)');
+            return $insertInfo->execute([
+                ':postTitle' => $_POST['postTitle'],
+                ':postContent' => $_POST['postContent']
+            ]);
+
+        }
+
+        return false;
+
+    }
+
 }
