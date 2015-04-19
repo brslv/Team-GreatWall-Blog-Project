fe48dffd2c68850671e2f6802b2ae26e414c073d
MeNinja <borislavkirilov@yahoo.com>
2015-04-19 14:43:25 +0300

    Added Current Date to Post

--- app/models/PostModel.php
+++ app/models/PostModel.php
@@ -5,10 +5,11 @@ class PostModel extends MainModel {
     public function addPost(){
 
         if(!empty($_POST['postTitle']) && !empty($_POST['postContent'])) {
-            $insertInfo = $this->db->prepare('INSERT INTO posts(title,content) VALUE (:postTitle,:postContent)');
+            $insertInfo = $this->db->prepare('INSERT INTO posts(title,content,publish_date) VALUE (:postTitle,:postContent,:postDate)');
             return $insertInfo->execute([
                 ':postTitle' => $_POST['postTitle'],
-                ':postContent' => $_POST['postContent']
+                ':postContent' => $_POST['postContent'],
+                ':postDate' => date('Y-m-d')
             ]);
 
         }
