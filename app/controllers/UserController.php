998205a40a7a548a2215d09872457f88e6767179
Katerina Noteva <knoteva@gmail.com>
2015-04-19 13:38:47 +0300

    Обновяване usercontroller

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -1,30 +1,35 @@
 <?php
-class User extends Main
-{
+class User extends Main {
 
     public function index()
     {
-        echo "Wrong move :)";
-        //$this->getView('homeView');
+        $this->getView('homeView');
+//            $this->getView('adminView');
     }
-    public function registerUser(){
-        $text = null;
-        if(isset($_POST['registerSubmit'])){
+
+    public function login() {
+        if (isset($_POST['loginSubmit'])) {
             $model = $this->getModel('UserModel');
-            $result = $model->register();
-            if($result){
-                $text = ['Registered successfully :)'];
-
+            $user = $model->login()[0];
+            if (!empty($user)){
+                $_SESSION['username'] = $user->username;
             }
-            else{
-                $text = ['Registered unsuccessfully :('];
-            }
-
         }
-
-        $this->getView('registerView',$text);
+        $this->getView('loginView');
     }
 
-
+    public function register() {
+        $message = null;
+        if (isset($_POST['registerSubmit'])) {
+            $model = $this->getModel('UserModel');
+            $user = $model->register();
+            if ($user) {
+                $message = ['Successull registry'];
+            } else {
+                $message = ['Register fail'];
+            }
+        }
+        $this->getView('registerView', $message);
+    }
 
 }
