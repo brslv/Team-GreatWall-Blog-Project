619eb117f998df5a934d19e020e6fc7227f63732
brslv <brslvg@gmail.com>
2015-04-20 11:07:17 +0300

    Added accidentally removed header templates.

---
+++ app/incl/header-tiny.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <meta charset="utf-8" />
+    <title>This is the blog.</title>
+    
+    <!-- HARDCODE THE STYLE URL: -->
+    <link rel="stylesheet" href="http://localhost/sites/SUProject/1/public/css/style.css" type="text/css" />
+<body>
+    <header class="header u-text-center">
+        <div class="container">
+            <nav class="top-nav nav">
+                <ul class="u-upper u-bold">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Contacts</a></li>
+                    <li><a href="http://localhost/GreatWall/trunk/public/user/login">Login</a></li>
+                    <li><a href="http://localhost/GreatWall/trunk/public/user/register">Register</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
