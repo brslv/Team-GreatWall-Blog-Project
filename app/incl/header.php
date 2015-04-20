52937529ce92d9ea5dbf27ed53c6ce4d0d7ec238
brslv <brslvg@gmail.com>
2015-04-20 11:54:03 +0300

    No source changes.

---
+++ app/incl/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <meta charset="utf-8" />
+    <title>This is the blog.</title>
+    
+    <!-- HARDCODE THE STYLE URL: -->
+    <link rel="stylesheet" href="http://localhost/sites/SUProject/1/public/css/style.css" type="text/css"  />
+<body>
+    <header class="header u-text-center">
+        <div class="container">
+            <nav class="top-nav nav">
+                <ul class="u-upper u-bold">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Contacts</a></li>
+                    <li><a href="http://localhost/GreatWall/trunk/public/user/login">Login</a></li>
+                    <li><a href="http://localhost/GreatWall/trunk/public/user/register">Register</a></li>
+                </ul>
+            </nav>
+            
+            <div class="welcome">
+                <h5 class="u-bold">Latest post: </h5></span>
+                <h1 class="u-bold"><a href="">Currently you are staring at the latest blog post.</a></h1>
+                <p>Published by <a href="">admin</a>.</p>
+            </div>
+        </div>
+    </header>
